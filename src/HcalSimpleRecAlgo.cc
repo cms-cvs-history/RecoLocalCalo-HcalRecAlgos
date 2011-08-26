64b99253bb96695ba2bf5a06aeb5d37d6c654253
Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch>
2011-08-26 22:54:30 +0000

    Add TDC reco

--- src/HcalSimpleRecAlgo.cc
+++ src/HcalSimpleRecAlgo.cc
@@ -1,4 +1,5 @@
 #include "RecoLocalCalo/HcalRecAlgos/interface/HcalSimpleRecAlgo.h"
+#include "RecoLocalCalo/HcalRecAlgos/src/HcalTDCReco.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "CalibCalorimetry/HcalAlgos/interface/HcalTimeSlew.h"
 #include <algorithm> // for "max"
@@ -133,11 +134,11 @@ HcalCalibRecHit HcalSimpleRecAlgo::reconstruct(const HcalCalibDataFrame& digi, i
 }
 
 HcalUpgradeRecHit HcalSimpleRecAlgo::reconstruct(const HcalUpgradeDataFrame& digi, int first, int toadd, const HcalCoder& coder, const HcalCalibrations& calibs) const {
-  return HcalSimpleRecAlgoImpl::reco<HcalUpgradeDataFrame,HcalUpgradeRecHit>(digi,coder,calibs,
-                                                                         first,toadd,correctForTimeslew_,
-                                                                         pulseCorr_.get(),
-                                                                         HcalTimeSlew::Medium,
-                                                                         setForData_ );
+  HcalUpgradeRecHit result = HcalSimpleRecAlgoImpl::reco<HcalUpgradeDataFrame,HcalUpgradeRecHit>(
+    digi,coder,calibs, first,toadd,correctForTimeslew_, pulseCorr_.get(), HcalTimeSlew::Medium,setForData_ );
+  HcalTDCReco tdcReco;
+  tdcReco.reconstruct(digi, result);
+  return result;
 }
 
 HFRecHit HcalSimpleRecAlgo::reconstruct(const HFDataFrame& digi, int first, int toadd, const HcalCoder& coder, const HcalCalibrations& calibs) const {
