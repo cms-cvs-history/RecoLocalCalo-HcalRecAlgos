988c522d8f7abf90d6b775cd5aad920f6033f1d7
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2012-07-20 14:56:27 +0000

    6bit TDC changes

--- src/HcalTDCReco.cc
+++ src/HcalTDCReco.cc
@@ -12,17 +12,19 @@ void HcalTDCReco::reconstruct(const HcalUpgradeDataFrame & digi,
   double risingTime = -999.;
   double fallingTime = -999.;
   int signalBX = 5;
-  for(int i=0; i < n; ++i)
+  // start at bunch crossing 3
+  for(int i=3; i < n; ++i)
   {
     unsigned tdc = digi.tdc(i);
-    unsigned rising = tdc & 0x1F;
-    unsigned falling = (tdc >> 5) & 0x1F;
+    unsigned rising = tdc & 0x7F;
+    // altered packing to tdcBins*2=7
+    unsigned falling = (tdc >> 7) & 0x7F;
     // only set the first time
-    if(risingTime < -998. && rising != 0 && rising != 31) {
-      risingTime = rising*25./32. + (i-signalBX)*25.;
+    if(risingTime < -998. && rising != 64 && rising != 65) {
+      risingTime = rising*25./64. + (i-signalBX)*25.;
     }
-    if(fallingTime < -998. && falling != 0 && falling != 31) {
-      fallingTime = falling*25./32. + (i-signalBX)*25.;
+    if(fallingTime < -998. && falling != 64 && falling != 65) {
+      fallingTime = falling*25./64. + (i-signalBX)*25.;
     }    
   }
   recHit = HcalUpgradeRecHit(recHit.id(), recHit.energy(), risingTime, fallingTime);
